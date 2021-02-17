38c0f130507bf9ac80cd8fcaa1b80306fcf5e9bc
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-17 22:39:17 +0100

    Add validation for user

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_02_17_212912) do
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
