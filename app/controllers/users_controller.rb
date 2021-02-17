5d10e18102b85d685da06440a531112bb3514ff0
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-17 23:40:41 +0100

    Adduser_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
-	@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+	#@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+	if @user.save
+		redirect_to new_users_path
+	else
+		render :new
+	end
+  end
 
-  if @user.save
-    redirect_to new_users_path
-  else
-    render :new
-  end
-  end
+  private
+	  def user_params
+	    params.require(:user).permit(:username,:email,:password, :authenticity_token)
+	end
 end
