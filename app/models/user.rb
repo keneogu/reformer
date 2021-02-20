dc470d5f3b8106ea952d77de0809fc0493bc222d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-20 17:47:22 +0100

    modified error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
 	validates :email, :password, presence: true
-	validates :username,
-    uniqueness: {
-      # object = person object being validated
-      # data = { model: "Person", attribute: "Username", value: <username> }
-      message: ->(object, data) do
-        "Hey #{object.username}, #{data[:value]} has already been taken."
-      end
-    }, presence: { message: "must be given please" }
+	validates :username, presence: { message: "must be given please" }
+	validates :username, on: :create, uniqueness: {
+		# object = person object being validated
+		# data = { model: "Person", attribute: "Username", value: <username> }
+		message: ->(object, data) do
+		  "Hey #{object.username}, #{data[:value]} has already been taken."
+		end
+	  }
 end
