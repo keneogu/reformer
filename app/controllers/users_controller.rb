45d9afe6d44d9472444323397472aebdd110f3c6
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-20 17:12:45 +0100

    Add error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,28 @@ class UsersController < ApplicationController
 	@user = User.new
   end
 
-  def edit
-	@user = User.find(params[:id])
-  end
-
   def create
 	#@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 	if @user.save
-		redirect_to new_users_path
+		redirect_to new_user_path
 	else
 		render :new
 	end
+  end
+
+  def edit
+	@user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
